b6fcfef09d360632028981d55f3a30ead098386d
Manu-Manzarek <alrachib@gmail.com>
2022-04-06 12:41:36 +0200

    Practica de Js

---
+++ app.js
@@ -0,0 +1,16 @@
+function escribirMensaje() {
+    console.log("Buenos Dias")
+}
+const otroMensaje = () => {
+    console.log("Otro Mensaje")
+}
+
+function escribirMensaje2(msj) {
+    console.log(msj)
+}
+
+function todo() {
+    escribirMensaje()
+    otroMensaje()
+    escribirMensaje2("Hola señora")
+}
